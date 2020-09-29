84c45c0cc0ca4274df275a8c450e71f3ad5c04fc
sduig <63716445+sduig@users.noreply.github.com>
2020-09-29 19:35:41 +0100

    Create gsa_advanced_proxy.py
    proxy support added

---
+++ gsa_advanced_proxy.py
@@ -0,0 +1,30 @@
+#/usr/bin/python3
+
+import atoma
+import requests
+import re
+import pandas as pd
+
+proxies = {
+      "http"="http://<<proxy>>:80"
+      "https"="https://<<proxy>>:80"
+      }
+
+response = requests.get('https://github.com/security-advisories', proxies=proxies)
+feed = atoma.parse_atom_bytes(response.content)
+
+new_items = []
+
+for vuln in feed.entries:
+    new_item = {}
+    new_item['Id'] = re.findall(r'\[(.*?)\]', vuln.title.value)
+    new_item['Published'] = vuln.published.strftime('%Y/%m/%d')
+    new_item['Updated'] = vuln.updated.strftime('%Y/%m/%d')
+    new_item['Title'] = re.findall(r'\s.*',vuln.title.value)
+    new_item['CVE'] = re.findall(r'CVE-\d{4}-\d{4,7}',vuln.content.value)
+    new_items.append(new_item)
+    print (new_items)
+
+df = pd.DataFrame(new_items,columns=['Id','Published','Updated','Title','Category','CVE'])
+df.head()
+df.to_csv('/home/<<user>>/GithubSA.csv',index=False, encoding = 'utf-8')
